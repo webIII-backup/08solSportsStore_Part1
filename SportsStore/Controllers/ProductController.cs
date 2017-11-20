8be630df8790024374121ea86a7817b9dcb097b5
Stefaan Samyn <stefaan.samyn@hogent.be>
2017-11-20 11:57:45 +0100

    Add Product/Index functionality

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models.Domain;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _productRepository.GetAll().OrderBy(b => b.Name).ToList();
+            return View(products);
+        }
+    }
+}
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SportsStore.Data;
 using Microsoft.EntityFrameworkCore;
+using SportsStore.Models.Domain;
+using SportsStore.Data.Repositories;
 
 namespace SportsStore
 {
@@ -25,6 +23,7 @@ namespace SportsStore
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddMvc();
         }
 
@@ -47,7 +46,7 @@ namespace SportsStore
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Product}/{action=Index}/{id?}");
             });
 
             SportsStoreDataInitializer.InitializeData(dbContext);
