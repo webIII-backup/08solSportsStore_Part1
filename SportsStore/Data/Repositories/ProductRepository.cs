7ea82fb998856539ce16d4b6b4dee396598b2d01
Stefaan Samyn <stefaan.samyn@hogent.be>
2017-11-20 11:41:25 +0100

    Add Product and Category repositories

---
+++ SportsStore/Data/Repositories/ProductRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsStore.Models.Domain;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<Product> _products;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _products = context.Products;
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _products.AsNoTracking().ToList();
+        }
+
+        public Product GetById(int productId)
+        {
+            return _products.Include(p => p.Category).SingleOrDefault(p => p.ProductId == productId);
+        }
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _products.Remove(product);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ SportsStore/Models/Domain/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SportsStore.Models.Domain
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAll();
+        Product GetById(int productId);
+        void Add(Product product);
+        void Delete(Product product);
+        void SaveChanges();
+    }
+}
